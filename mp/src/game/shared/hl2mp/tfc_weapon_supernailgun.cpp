ca1c63854601983f3dab6cd1ab1ac73c5978c4d8
Stephen henson <hensonstephen61@gmail.com>
2019-10-27 17:03:45 -0400

    Added new weapons/defs to stubs class.

---
+++ mp/src/game/shared/hl2mp/tfc_weapon_supernailgun.cpp
@@ -0,0 +1,295 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose: 
+//
+//=============================================================================//
+
+#include "cbase.h"
+#include "npcevent.h"
+#include "in_buttons.h"
+
+#ifdef CLIENT_DLL
+#include "c_hl2mp_player.h"
+#else
+#include "grenade_ar2.h"
+#include "hl2mp_player.h"
+#include "basegrenade_shared.h"
+#endif
+
+#include "weapon_hl2mpbase.h"
+#include "weapon_hl2mpbase_machinegun.h"
+
+#ifdef CLIENT_DLL
+#define CWeaponSMG1 C_WeaponSMG1
+#endif
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+#define SMG1_GRENADE_DAMAGE 100.0f
+#define SMG1_GRENADE_RADIUS 250.0f
+
+class CWeaponSuperNailGun : public CHL2MPMachineGun
+{
+public:
+	DECLARE_CLASS(CWeaponSuperNailGun, CHL2MPMachineGun);
+
+	CWeaponSuperNailGun();
+
+	DECLARE_NETWORKCLASS();
+	DECLARE_PREDICTABLE();
+
+
+	void	Precache(void);
+	void	AddViewKick(void);
+	virtual void	PrimaryAttack();
+	void	SecondaryAttack(void);
+
+	int		GetMinBurst() { return 2; }
+	int		GetMaxBurst() { return 5; }
+
+	virtual void Equip(CBaseCombatCharacter *pOwner);
+	bool	Reload(void);
+
+	float	GetFireRate(void) { return 0.075f; }	// 13.3hz
+	Activity	GetPrimaryAttackActivity(void);
+
+	virtual const Vector& GetBulletSpread(void)
+	{
+		static const Vector cone = VECTOR_CONE_5DEGREES;
+		return cone;
+	}
+
+	const WeaponProficiencyInfo_t *GetProficiencyValues();
+
+#ifndef CLIENT_DLL
+	DECLARE_ACTTABLE();
+#endif
+
+protected:
+
+	Vector	m_vecTossVelocity;
+	float	m_flNextGrenadeCheck;
+
+private:
+	CWeaponSuperNailGun(const CWeaponSuperNailGun &);
+};
+
+IMPLEMENT_NETWORKCLASS_ALIASED(WeaponSuperNailGun, DT_WeaponSuperNailGun)
+
+BEGIN_NETWORK_TABLE(CWeaponSuperNailGun, DT_WeaponSuperNailGun)
+END_NETWORK_TABLE()
+
+BEGIN_PREDICTION_DATA(CWeaponSuperNailGun)
+END_PREDICTION_DATA()
+
+LINK_ENTITY_TO_CLASS(tfc_weapon_supernailgun, CWeaponSuperNailGun);
+PRECACHE_WEAPON_REGISTER(tfc_weapon_supernailgun);
+
+#ifndef CLIENT_DLL
+acttable_t	CWeaponSuperNailGun::m_acttable[] =
+{
+	{ ACT_HL2MP_IDLE, ACT_HL2MP_IDLE_SMG1, false },
+	{ ACT_HL2MP_RUN, ACT_HL2MP_RUN_SMG1, false },
+	{ ACT_HL2MP_IDLE_CROUCH, ACT_HL2MP_IDLE_CROUCH_SMG1, false },
+	{ ACT_HL2MP_WALK_CROUCH, ACT_HL2MP_WALK_CROUCH_SMG1, false },
+	{ ACT_HL2MP_GESTURE_RANGE_ATTACK, ACT_HL2MP_GESTURE_RANGE_ATTACK_SMG1, false },
+	{ ACT_HL2MP_GESTURE_RELOAD, ACT_HL2MP_GESTURE_RELOAD_SMG1, false },
+	{ ACT_HL2MP_JUMP, ACT_HL2MP_JUMP_SMG1, false },
+	{ ACT_RANGE_ATTACK1, ACT_RANGE_ATTACK_SMG1, false },
+};
+
+IMPLEMENT_ACTTABLE(CWeaponSuperNailGun);
+#endif
+
+//=========================================================
+CWeaponSuperNailGun::CWeaponSuperNailGun()
+{
+	m_fMinRange1 = 0;// No minimum range. 
+	m_fMaxRange1 = 1400;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponSuperNailGun::Precache(void)
+{
+#ifndef CLIENT_DLL
+	UTIL_PrecacheOther("grenade_ar2");
+#endif
+
+	BaseClass::Precache();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Give this weapon longer range when wielded by an ally NPC.
+//-----------------------------------------------------------------------------
+void CWeaponSuperNailGun::Equip(CBaseCombatCharacter *pOwner)
+{
+	m_fMaxRange1 = 1400;
+
+	BaseClass::Equip(pOwner);
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Output : Activity
+//-----------------------------------------------------------------------------
+Activity CWeaponSuperNailGun::GetPrimaryAttackActivity(void)
+{
+	if (m_nShotsFired < 2)
+		return ACT_VM_PRIMARYATTACK;
+
+	if (m_nShotsFired < 3)
+		return ACT_VM_RECOIL1;
+
+	if (m_nShotsFired < 4)
+		return ACT_VM_RECOIL2;
+
+	return ACT_VM_RECOIL3;
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+bool CWeaponSuperNailGun::Reload(void)
+{
+	bool fRet;
+	float fCacheTime = m_flNextSecondaryAttack;
+
+	fRet = DefaultReload(GetMaxClip1(), GetMaxClip2(), ACT_VM_RELOAD);
+	if (fRet)
+	{
+		// Undo whatever the reload process has done to our secondary
+		// attack timer. We allow you to interrupt reloading to fire
+		// a grenade.
+		m_flNextSecondaryAttack = GetOwner()->m_flNextAttack = fCacheTime;
+
+		WeaponSound(RELOAD);
+	}
+
+	return fRet;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponSuperNailGun::AddViewKick(void)
+{
+#define	EASY_DAMPEN			0.5f
+#define	MAX_VERTICAL_KICK	1.0f	//Degrees
+#define	SLIDE_LIMIT			2.0f	//Seconds
+
+	//Get the view kick
+	CBasePlayer *pPlayer = ToBasePlayer(GetOwner());
+
+	if (pPlayer == NULL)
+		return;
+
+	DoMachineGunKick(pPlayer, EASY_DAMPEN, MAX_VERTICAL_KICK, m_fFireDuration, SLIDE_LIMIT);
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponSuperNailGun::PrimaryAttack(void)
+{
+	// Only the player fires this way so we can cast
+	CBasePlayer *pPlayer = ToBasePlayer(GetOwner());
+	if (!pPlayer)
+		return;
+
+	// Abort here to handle burst and auto fire modes
+	if ((UsesClipsForAmmo1() && m_iClip1 == 0) || (!UsesClipsForAmmo1() && !pPlayer->GetAmmoCount(m_iPrimaryAmmoType)))
+		return;
+
+	m_nShotsFired++;
+
+	pPlayer->DoMuzzleFlash();
+
+	// To make the firing framerate independent, we may have to fire more than one bullet here on low-framerate systems, 
+	// especially if the weapon we're firing has a really fast rate of fire.
+	int iBulletsToFire = 0;
+	float fireRate = GetFireRate();
+
+	while (m_flNextPrimaryAttack <= gpGlobals->curtime)
+	{
+		// MUST call sound before removing a round from the clip of a CHLMachineGun
+		WeaponSound(SINGLE, m_flNextPrimaryAttack);
+		m_flNextPrimaryAttack = m_flNextPrimaryAttack + fireRate;
+		iBulletsToFire++;
+	}
+
+	// Make sure we don't fire more than the amount in the clip, if this weapon uses clips
+	if (UsesClipsForAmmo1())
+	{
+		if (iBulletsToFire > m_iClip1)
+			iBulletsToFire = m_iClip1;
+		m_iClip1 -= iBulletsToFire;
+	}
+
+	CHL2MP_Player *pHL2MPPlayer = ToHL2MPPlayer(pPlayer);
+
+	// Fire the Nails
+
+	Vector vecSrc = pPlayer->Weapon_ShootPosition();
+	Vector	vecThrow;
+	// Don't autoaim on grenade tosses
+	AngleVectors(pPlayer->EyeAngles() + pPlayer->GetPunchAngle(), &vecThrow);
+	VectorScale(vecThrow, 1000.0f, vecThrow);
+
+#ifndef CLIENT_DLL
+	//Create the grenade
+	CGrenadeAR2 *pGrenade = (CGrenadeAR2*)Create("grenade_ar2", vecSrc, vec3_angle, pPlayer);
+	pGrenade->SetAbsVelocity(vecThrow);
+
+	pGrenade->SetLocalAngularVelocity(RandomAngle(-400, 400));
+	pGrenade->SetMoveType(MOVETYPE_FLYGRAVITY, MOVECOLLIDE_FLY_BOUNCE);
+	pGrenade->SetThrower(GetOwner());
+	pGrenade->SetDamage(SMG1_GRENADE_DAMAGE);
+	pGrenade->SetDamageRadius(SMG1_GRENADE_RADIUS);
+#endif
+
+	//Factor in the view kick
+	AddViewKick();
+
+	if (!m_iClip1 && pPlayer->GetAmmoCount(m_iPrimaryAmmoType) <= 0)
+	{
+		// HEV suit - indicate out of ammo condition
+		pPlayer->SetSuitUpdate("!HEV_AMO0", FALSE, 0);
+	}
+
+	SendWeaponAnim(GetPrimaryAttackActivity());
+	pPlayer->SetAnimation(PLAYER_ATTACK1);
+
+	// Decrease ammo
+	if (UsesClipsForAmmo1())
+	{
+		pPlayer->RemoveAmmo(1, m_iClip1);
+	}
+	else
+	{
+		pPlayer->RemoveAmmo(1, m_iPrimaryAmmoType);
+	}
+
+
+	// Can shoot again immediately
+	m_flNextPrimaryAttack = gpGlobals->curtime + 0.5f;
+
+}
+
+//-----------------------------------------------------------------------------
+const WeaponProficiencyInfo_t *CWeaponSuperNailGun::GetProficiencyValues()
+{
+	static WeaponProficiencyInfo_t proficiencyTable[] =
+	{
+		{ 7.0, 0.75 },
+		{ 5.00, 0.75 },
+		{ 10.0 / 3.0, 0.75 },
+		{ 5.0 / 3.0, 0.75 },
+		{ 1.00, 1.0 },
+	};
+
+	COMPILE_TIME_ASSERT(ARRAYSIZE(proficiencyTable) == WEAPON_PROFICIENCY_PERFECT + 1);
+
+	return proficiencyTable;
+}

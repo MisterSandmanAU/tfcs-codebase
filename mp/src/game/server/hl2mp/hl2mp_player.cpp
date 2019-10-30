396288dbe18d826c8687a52273b353c80495054c
Stephen henson <hensonstephen61@gmail.com>
2019-10-30 15:19:42 -0400

    Revert "Merge branch 'master' of https://github.com/SandvichThief/tfcs-codebase"
    This reverts commit 4603186f300f2892c9d813fa68d077ad4847cbda, reversing
    changes made to dee4e596128c5fd727b06f8e3bf5b1bbed7bc0e9.

--- mp/src/game/server/hl2mp/hl2mp_player.cpp
+++ mp/src/game/server/hl2mp/hl2mp_player.cpp
@@ -306,7 +306,7 @@ void CHL2MP_Player::Spawn(void)
 	m_flNextModelChangeTime = 0.0f;
 	m_flNextTeamChangeTime = 0.0f;
 
-	PickDefaultSpawnTeam();
+	//PickDefaultSpawnTeam();
 	
 
 	BaseClass::Spawn();
@@ -928,7 +928,6 @@ void CHL2MP_Player::ChangeTeam( int iTeam )
 	}*/
 
 	bool bKill = false;
-	bool bTeamPlayer = true;
 
 	if ( HL2MPRules()->IsTeamplay() != true && iTeam != TEAM_SPECTATOR )
 	{
@@ -941,10 +940,6 @@ void CHL2MP_Player::ChangeTeam( int iTeam )
 		if ( iTeam != GetTeamNumber() && GetTeamNumber() != TEAM_UNASSIGNED )
 		{
 			bKill = true;
-		}
-		if( GetTeamNumber() < TEAM_RED )
-		{
-			bTeamPlayer = false;
 		}
 	}
 
@@ -971,10 +966,6 @@ void CHL2MP_Player::ChangeTeam( int iTeam )
 	if ( bKill == true )
 	{
 		CommitSuicide();
-	}
-	if( bTeamPlayer == false )
-	{
-		ForceRespawn();
 	}
 }
 
